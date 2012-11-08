61962eff4174b7654ea28ff1d19f7527e050ba4b
Stan Tarnovsky <starnovsky@gmail.com>
2012-11-07 16:46:20 -0800

    created

---
+++ proxy.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.ServiceModel.Activation;
+using System.Net;
+using System.IO;
+
+namespace JSONPproxy
+{
+    [ServiceContract]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ProxyService 
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public string Hello()
+        {
+            return "Hello World!";
+        }
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public string GrabHtml(string url)
+        {
+            WebRequest req = WebRequest.Create(url);
+            using (WebResponse rsp = req.GetResponse())
+            {
+                using (StreamReader sr = new StreamReader(rsp.GetResponseStream()))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+        }
+        
+    }
+}
